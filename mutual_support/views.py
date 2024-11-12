e20dfbbc00703e8a7500f925082959ab8e95473b
NeiraJ <johnny.neira@gmail.com>
2024-11-12 10:20:15 +0100

    build: Ajout des vues de base pour l'authentification et l'enregistrement des utilisateurs
    - Ajout de la vue index
    - Implémentation de la vue login_view
    - Création de la vue logout_view
    - Mise en place de la vue signup

--- mutual_support/views.py
+++ mutual_support/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 
-# Create your views here.
+INDEX_PAGE = 'mutual_support:index'
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect(INDEX_PAGE)
+    return render(request, 'login.html')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(INDEX_PAGE)
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect(INDEX_PAGE)
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
