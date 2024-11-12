849008219deb8f86f4c26abe7bfc288905e4e749
NeiraJ <johnny.neira@gmail.com>
2024-11-12 14:42:01 +0100

    Enregistrement des modèles dans l'interface administrative Django
    - Ajout de Competence, Profile et Creneau dans l'interface admin Django

--- mutual_support/admin.py
+++ mutual_support/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+
+from .models import Competence
+from .models import Profile
+from .models import Creneau
+
+
+admin.site.register(Competence)
+admin.site.register(Profile)
+admin.site.register(Creneau)
+
