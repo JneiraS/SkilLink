0826a80dad8f3cf2ae649366d113515be7320d5a
NeiraJ <johnny.neira@gmail.com>
2024-11-12 11:22:58 +0100

    Ajouter modèle de compétences
    - Implémente modèle Competence pour gérer les compétences des utilisateurs
    - Ajoute champ category pour regrouper les compétences par catégorie
    - méthode __str__ pour la classe Competence

--- mutual_support/models.py
+++ mutual_support/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
-from django.db import models
-
 
 class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
     email = models.EmailField()
+
+
+class Competence(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    category = models.CharField(max_length=100, blank=True,
+                                null=True)  # Optionnel, pour regrouper par catégorie
+
+    def __str__(self):
+        return self.name
