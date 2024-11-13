a22b77459204a3f372749c87de2ee703c43868d2
NeiraJ <johnny.neira@gmail.com>
2024-11-13 09:02:53 +0100

    Ajout d'une nouvelle URL pour le formulaire d'offres
    - Ajout de la route 'offers-form/' avec le nom 'offers_form'

--- mutual_support/urls.py
+++ mutual_support/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.signup, name='signup')
+    path('', views.signup, name='signup'),
+    path('offers-form/', views.offers_form_view, name='offers_form')
 ]
