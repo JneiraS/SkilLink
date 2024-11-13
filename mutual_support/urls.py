2e72b644140e0ac3ee4ac1207bdaa90cfc72fbda
NeiraJ <johnny.neira@gmail.com>
2024-11-13 16:24:09 +0100

    Ajout d'une nouvelle URL pour le profil utilisateur
    - Création de la route 'profile/<int:user_id>' avec le nom 'profile'
    - Création de la route 'signup/' avec le nom 'signup'

--- mutual_support/urls.py
+++ mutual_support/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
-    path('', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup'),
     path('offers-form/', views.offers_form_view, name='offers_form'),
     path('request-form/', views.assistance_request_view, name='request_form'),
     path('offer/<int:creneau_id>/', views.offer_view, name='offer'),
+    path('profile/<int:user_id>', views.profile_view, name='profile'),
 ]
