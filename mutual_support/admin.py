69cf8dace5183c221c43da8aeed23d53ba835a40
NeiraJ <johnny.neira@gmail.com>
2024-11-13 16:22:34 +0100

    Ajout de UserCompetence à l'interface d'administration
    - Importation et enregistrement de UserCompetence dans l'interface admin

--- mutual_support/admin.py
+++ mutual_support/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Competence
+from .models import Competence, UserCompetence
 from .models import Profile
 from .models import Creneau
 
@@ -11,4 +11,5 @@ from .models import Creneau
 admin.site.register(Competence)
 admin.site.register(Profile)
 admin.site.register(Creneau)
+admin.site.register(UserCompetence)
 
