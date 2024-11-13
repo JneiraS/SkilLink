8be175556a5d66db8365a2a1e10ffd0f0722c5ae
NeiraJ <johnny.neira@gmail.com>
2024-11-13 09:03:15 +0100

    Mise à jour de la vue index et ajout d'une nouvelle vue pour le formulaire d'offres
    - Ajout des offres dans le contexte de la vue index
    - Création d'une nouvelle fonction offers_form_view pour gérer le formulaire d'offres

--- mutual_support/views.py
+++ mutual_support/views.py
@@ -2,16 +2,20 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
-from mutual_support.models import Competence
+from mutual_support.forms import CreneauForm
+from mutual_support.models import Competence, Creneau
 
 INDEX_PAGE = 'mutual_support:index'
 
 
 def index(request):
     categories = Competence.objects.values_list('category', flat=True).distinct()
-    print(categories)
+    offers = Creneau.objects.all().order_by('date')
+    current_user = request.user
+    print(offers[0].competence)
     context = {
-        'categories': categories
+        'categories': categories,
+        'offers': offers
     }
     return render(request, 'index.html', context)
 
@@ -45,3 +49,17 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'signup.html', {'form': form})
+
+
+def offers_form_view(request):
+    if request.method == 'POST':
+        form = CreneauForm(request.POST)
+        if form.is_valid():
+            current_user = request.user
+            form.instance.user = current_user
+            form.save()
+            return redirect(INDEX_PAGE)
+    else:
+        form = CreneauForm()  # This line is not the issue
+
+    return render(request, 'offers-form.html', {'form': form})  # The form variable is used here
