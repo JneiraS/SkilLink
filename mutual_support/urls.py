39f25341e4621e54db98eb9611fe06c1aa4f2c77
NeiraJ <johnny.neira@gmail.com>
2024-11-13 10:17:12 +0100

    Ajout d'une nouvelle URL pour afficher les détails d'un créneau
    - Ajout de la route 'offer/<int:creneau_id>/' avec le nom 'offer'

--- mutual_support/urls.py
+++ mutual_support/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('', views.signup, name='signup'),
-    path('offers-form/', views.offers_form_view, name='offers_form')
+    path('offers-form/', views.offers_form_view, name='offers_form'),
+    path('offer/<int:creneau_id>/', views.offer_view, name='offer'),
 ]
